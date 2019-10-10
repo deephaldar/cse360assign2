e27714725d8585928b7dbd2352615a7fce6cc5a2
Soumya Deep Haldar <d.haldar@hotmail.com>
2019-10-09 21:49:41 -0700

    Updated methods!

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -4,28 +4,39 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total; 
+	private String history;
 	
+	//Initializes the required variables used throughout the program
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
+		history = "0"; //sets the string of history to 0
 	}
 	
+	//Returns the total
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
+	//A certain value of transaction is added to the current total
 	public void add (int value) {
-		
+		total += value;
+		history = history + " + " + value;
 	}
 	
+	//A certain value of transaction is subtracted from the current total
 	public void subtract (int value) {
-		
+		total -= value;
+		history = history + " - " + value;
 	}
-		
+	
+	//Returns all the transaction history from 0
 	public String toString () {
-		return "";
+		return history;
 	}
 
+	//Clears the history and total by setting them back to 0
 	public void clear() {
-	
+		total = 0;
+		history = "0";
 	}
 }
